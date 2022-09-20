5b3b6a58c882e46326aa255687bedcea127f39af
mao <1296193245@qq.com>
2022-09-20 12:45:55 +0800

    android_simultaneous_display_of_text_and_images

---
+++ app/src/main/java/mao/android_simultaneous_display_of_text_and_images/MainActivity.java
@@ -0,0 +1,16 @@
+package mao.android_simultaneous_display_of_text_and_images;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
